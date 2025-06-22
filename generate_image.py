c50fd75165251579c2a86adb68ac849b9372c5ca
Yash Shah <yashshah00@outlook.com>
2025-06-22 23:02:32 +0100

    Updated image generation code.

--- generate_image.py
+++ generate_image.py
@@ -7,7 +7,8 @@ from main import Hyperparameters
 from modules import UNet2DDiffusionModel
 
 model = UNet2DDiffusionModel.load_from_checkpoint(
-    "./diffusion/4gsxqc4s/checkpoints/epoch=9-step=15630.ckpt"
+    # "./diffusion/4gsxqc4s/checkpoints/epoch=9-step=15630.ckpt"
+    "./diffusion/7w5tmyq4/checkpoints/epoch=199-step=312600.ckpt"
 )
 hyperparameters = Hyperparameters()
 
@@ -21,27 +22,29 @@ scheduler.set_timesteps(hyperparameters.num_timesteps)  # Number of inference st
 
 in_channels = 3
 sample_size = 32
+num_images = 32
 
-# Start from pure noise
-sample_shape = (
-    1,
-    in_channels,
-    sample_size,
-    sample_size,
-)
-image = torch.randn(sample_shape).to(model.device)
+for i in range(num_images):
+    # Start from pure noise
+    sample_shape = (
+        1,
+        in_channels,
+        sample_size,
+        sample_size,
+    )
+    image = torch.randn(sample_shape).to(model.device)
 
-for t in scheduler.timesteps:
-    # Predict the noise residual
-    with torch.no_grad():
-        noise_pred = model(image, t).sample
+    for t in scheduler.timesteps:
+        # Predict the noise residual
+        with torch.no_grad():
+            noise_pred = model(image, t).sample
 
-    # Compute the previous image x_{t-1}
-    image = scheduler.step(noise_pred, t, image).prev_sample
+        # Compute the previous image x_{t-1}
+        image = scheduler.step(noise_pred, t, image).prev_sample
 
-# Convert to image (from [-1, 1] to [0, 255])
-image = (image.clamp(-1, 1) + 1) / 2  # [0,1]
-image = image.cpu().permute(0, 2, 3, 1).numpy()[0]  # CHW -> HWC
-image = (image * 255).astype(np.uint8)
+    # Convert to image (from [-1, 1] to [0, 255])
+    image = (image.clamp(-1, 1) + 1) / 2  # [0,1]
+    image = image.cpu().permute(0, 2, 3, 1).numpy()[0]  # CHW -> HWC
+    image = (image * 255).astype(np.uint8)
 
-Image.fromarray(image).save("generated.png")
+    Image.fromarray(image).save(f"generated_{i}.png")
